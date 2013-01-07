bc6e80f2baf6e20cda16faff6532eefab982ae21
e7mac <mayank.ot@gmail.com>
2013-01-06 21:35:11 -0800

    updated readme

---
+++ Chip/DelayChip.cpp
@@ -0,0 +1,9 @@
+//
+//  DelayChip.cpp
+//  ChipLibraryTestBed
+//
+//  Created by Kurt Werner on 11/25/12.
+//  Copyright (c) 2012 Mayank, Kurt. All rights reserved.
+//
+
+#include "DelayChip.h"
---
+++ Chip/DelayChip.h
@@ -0,0 +1,14 @@
+//
+//  DelayChip.h
+//  ChipLibraryTestBed
+//
+//  Created by Kurt Werner on 11/25/12.
+//  Copyright (c) 2012 Mayank, Kurt. All rights reserved.
+//
+
+#ifndef __ChipLibraryTestBed__DelayChip__
+#define __ChipLibraryTestBed__DelayChip__
+
+#include <iostream>
+
+#endif /* defined(__ChipLibraryTestBed__DelayChip__) */
